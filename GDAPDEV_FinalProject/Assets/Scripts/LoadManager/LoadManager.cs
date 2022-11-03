b5e49205a8ff9195b46de48dc0b4b5b952ffa207
Kappa <kevin_carl_capalar@dlsu.edu.ph>
2022-11-03 15:46:11 +0800

    adding event broadcasting and scene management
    For future development there will an event broadcasting

---
+++ GDAPDEV_FinalProject/Assets/Scripts/LoadManager/LoadManager.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+/// <summary>
+/// Convenience function for loading a new scene while cleaning up the existing scene
+/// </summary>
+public class LoadManager {
+	private static LoadManager sharedInstance = null;
+	public static LoadManager Instance {
+		get {
+			if (sharedInstance == null) {
+				sharedInstance = new LoadManager ();
+			}
+
+			return sharedInstance;
+		}
+	}
+
+	/// <summary>
+	/// Loads a new scene
+	/// </summary>
+	/// <param name="name">Name.</param>
+	/// <param name="clearEventBroadcast">If set to <c>true</c> clears all attached observers and events in the broadcast system.</param>
+	public void LoadScene(string name, bool clearEventBroadcast = true) {
+		if (clearEventBroadcast) {
+			EventBroadcaster.Instance.RemoveAllObservers (); //remove all observers associated with the scene
+		}
+
+		SceneManager.LoadScene (name);
+
+	}
+
+	/// <summary>
+	/// Loads a new scene but does not destroy the old scene. Game objects will be added.
+	/// </summary>
+	/// <param name="name">Name.</param>
+	/// <param name="clearEventBroadcast">If set to <c>true</c> clears all attached observers and events in the broadcast system.</param>
+	public void LoadSceneAdditive(string name, bool clearEventBroadcast = true) {
+		if (clearEventBroadcast) {
+			EventBroadcaster.Instance.RemoveAllObservers (); //remove all observers associated with the scene
+		}
+
+		SceneManager.LoadScene (name, LoadSceneMode.Additive);
+	}
+}
---
+++ GDAPDEV_FinalProject/Assets/Scripts/LoadManager/SceneNames.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+/// <summary>
+/// Contains all scene names required
+/// By: Neil DG
+/// </summary>
+public class SceneNames {
+	public const string MAIN_SCENE = "MainScene";
+	//Format
+	// public const string <CODE_NAME_SCENE> = "ActualSceneName";
+}
